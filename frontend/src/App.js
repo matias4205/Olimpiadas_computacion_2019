21c62ebc4217368ce877a14b7121e5973823356f
matias4205 <matiasperezpc@hotmail.com>
2019-08-21 15:43:11 -0300

    Navbar color changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import SideBar from './components/SideBar'
 import Header from './components/Header';
 import Content from './components/Content';
 
+import './assets/styles/general.css';
+
 function App() {
   return (
     <div>
