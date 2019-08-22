1ae0dae92bf447c85de0987c2ab781115e8b4ade
Santiago Moran <santimoran.1@hotmail.com>
2019-08-22 18:45:29 -0300

    Routes profile, quick dashboard and 404 created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
 
-import SideBar from './components/SideBar'
-import Header from './components/Header';
-import Content from './components/Content';
+import Layout from "./components/Layout";
+import Profile from './pages/Profile/containers/Profile';
+import QuickView from './pages/Dashboard/Quick/containers/QuickView'
 
 import './assets/styles/general.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <SideBar />
-      <Content />
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" render={() => (<Redirect to='/dashboard/quick'/>)}/>
+          <Route exact path="/profile/:userId" component={Profile} />
+          <Route exact path="/dashboard/quick" component={QuickView} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
