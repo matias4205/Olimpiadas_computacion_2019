48baeb256d3ed6fe8267cf67bfb5ea589148e831
Santiago Moran <santimoran.1@hotmail.com>
2019-08-22 22:51:28 -0300

    Merge routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect
+}from 'react-router-dom';
 
-import SideBar from './components/SideBar'
-import Header from './components/Header';
-import Content from './components/Content';
+import Layout from "./components/Layout";
+import Profile from './pages/Profile/containers/Profile';
+import QuickView from './pages/Dashboard/Quick/containers/QuickView'
+import Management from './pages/Management/containers/Management';
 
 import './assets/styles/general.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <SideBar />
-      <Content />
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" render={() => (<Redirect to='/dashboard/quick'/>)}/>
+          <Route exact path="/sections" component={Management}/>
+          <Route exact path="/profile/:userId" component={Profile} />
+          <Route exact path="/dashboard/quick" component={QuickView} />
+          <Route>404 Not Found</Route>
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Management/containers/Management.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Management extends Component {
+    render() {
+        return(
+            <React.Fragment>
+                sa
+            </React.Fragment>
+        );
+    }
+}
+
+export default Management;
