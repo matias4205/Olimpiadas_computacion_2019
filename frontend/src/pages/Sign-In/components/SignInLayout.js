fe35248b46ee197f81a8658c0ff18a61f56b8e49
Santiago Moran <santimoran.1@hotmail.com>
2019-08-31 03:29:30 -0300

    Sign In/Up almost ready, should correct its fields and add a company form

--- frontend/src/pages/Sign-In/components/SignInLayout.js
+++ frontend/src/pages/Sign-In/components/SignInLayout.js
@@ -1,45 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import ContentHeader from '../../../components/ContentHeader';
+import '../../../assets/styles/general.css';
+import '../../Sign-Up/components/styles/styles.css';
 
-const SignInLayout = () => {
+const SignInLayout = (props) => {
     return (
-        <React.Fragment>
-            <ContentHeader title="Sign in" subtitle="Access to my account" />
-                    <div className="col-md-6">
-                        <div className="box box-info">
-                            <div className="box-header with-border">
-                                <h3>Sign in</h3>
-                            </div>
-                            <div class="box-body">
-                                <div class="form-group">
-                                    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
-                                    <div class="col-sm-10">
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email"/>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
-                                    <div class="col-sm-10">
-                                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password"/>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="col-sm-offset-2 col-sm-10">
-                                        <div class="checkbox">
-                                            <label>
-                                                <input type="checkbox"/> Remember me
-                                            </label>
-                                        </div>
-                                    </div>
+        <div className="full-height-container login-container">
+            <div className="login-box no-margin">
+                <div className="login-logo">
+                    <p className="text-white">Sign In</p>
+                </div>
+                <div className="login-box-body">
+                    <p className="login-box-msg">Sign in to start your session</p>
+                    <form action="../../index2.html" method="post">
+                        <div className="form-group has-feedback">
+                            <input type="email" className="form-control" placeholder="Email" />
+                            <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
+                        </div>
+                        <div className="form-group has-feedback">
+                            <input type="password" className="form-control" placeholder="Password" />
+                            <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-8">
+                                <div class="checkbox icheck">
+                                    <label>
+                                        <input type="checkbox" /> Remember Me
+                                    </label>
                                 </div>
                             </div>
-                            <div class="box-footer">
-                                <button type="submit" class="btn btn-info pull-right">Sign in</button>
+                            <div className="col-xs-4">
+                                <button type="submit" className="btn btn-primary btn-block btn-flat">Sign In</button>
                             </div>
+                        </div>
+                    </form>
+                    <div className="social-auth-links text-center">
+                        <p>- OR -</p>
+                        <a href="#" className="btn btn-block btn-social btn-facebook btn-flat"><i className="fa fa-facebook"></i> Sign in using Facebook</a>
+                        <a href="#" className="btn btn-block btn-social btn-google btn-flat"><i className="fa fa-google-plus"></i> Sign in using Google+</a>
+                    </div>
+                    <a href="#">I forgot my password</a>
+                    <br />
+                    <Link to="/sign-up" className="text-center">Register a new membership</Link>
                 </div>
             </div>
-        </React.Fragment>
+        </div>
     );
 };
 
--- frontend/src/pages/Sign-Up/components/SignUpLayout.js
+++ frontend/src/pages/Sign-Up/components/SignUpLayout.js
@@ -1,69 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../../../assets/styles/general.css';
+import './styles/styles.css';
 
 const SignUpLayout = (props) => {
     return (
-        <React.Fragment>
-            <div className="content">
-                <div className="row">
-                    <div className="col-md-6">
-                       <div className="box box-info">
-                            <div className="box-header with-border">
-                                <h3>Personal data</h3>
-                            </div>
-                            <div class="box-body">
-                                <div class="form-group">
-                                    <label>First name</label>
-                                    <input type="text" class="form-control" placeholder="First name"/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Last name</label>
-                                    <input type="email" class="form-control" placeholder="Last name"/>                                
-                                </div>
-                                <div class="form-group">
-                                    <label>Email</label>
-                                    <input type="email" class="form-control" placeholder="Email"/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Password</label>
-                                    <input type="password" class="form-control" placeholder="Password"/>                                
-                                </div>
-                            </div>
-                            <div class="box-footer">
-                                <button type="submit" class="btn btn-info pull-right">Sign in</button>
-                            </div>
+        <div className="full-height-container login-container">
+            <div className="login-box no-margin">
+                <div className="register-logo">
+                   <p className="text-white">Register</p>
+                </div>
+                <div className="login-box-body">
+                    <p className="login-box-msg">Register a new membership</p>
+                    <form action="../../index.html" method="post">
+                        <div className="form-group has-feedback">
+                            <input type="text" className="form-control" placeholder="Full name" />
+                            <span className="glyphicon glyphicon-user form-control-feedback"></span>
                         </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="box box-info">
-                            <div className="box-header with-border">
-                                <h3>Company</h3>
-                            </div>
-                            <div class="box-body">
-                                <div class="form-group">
-                                    <label>Comercial denomination</label>
-                                    <input type="text" class="form-control" placeholder="Ccial denomination"/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Owner company</label>
-                                    <input type="text" class="form-control" placeholder="Owner company"/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Fiscal code</label>                                
-                                    <input type="text" class="form-control" placeholder="Fiscal code"/>
-                                </div>
-                                <div class="form-group">
-                                    <label>Address</label>
-                                    <input type="text" class="form-control" placeholder="Address"/>
-                                </div>
-                            </div>
-                            <div class="box-footer">
-                                <button type="submit" class="btn btn-info pull-right">Sign in</button>
-                            </div>
+                        <div className="form-group has-feedback">
+                            <input type="email" className="form-control" placeholder="Email" />
+                            <span className="glyphicon glyphicon-envelope form-control-feedback"></span>
                         </div>
+                        <div className="form-group has-feedback">
+                            <input type="password" className="form-control" placeholder="Password" />
+                            <span className="glyphicon glyphicon-lock form-control-feedback"></span>
+                        </div>
+                        <div className="form-group has-feedback">
+                            <input type="password" className="form-control" placeholder="Retype password" />
+                            <span className="glyphicon glyphicon-log-in form-control-feedback"></span>
+                        </div>
+                    </form>
+                    <div className="social-auth-links text-center">
+                        <p>- OR -</p>
+                        <a href="#" className="btn btn-block btn-social btn-facebook btn-flat"><i className="fa fa-facebook"></i> Sign up using Facebook</a>
+                        <a href="#" className="btn btn-block btn-social btn-google btn-flat"><i className="fa fa-google-plus"></i> Sign up using Google+</a>
                     </div>
+                    <Link to="/sign-in" className="text-center">I already have a membership</Link>
                 </div>
             </div>
-        </React.Fragment>
+        </div>
     );
 };
 
