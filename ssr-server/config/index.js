233f4d46f284f58444559523a0c89e237f457194
matias4205 <matiasperezpc@hotmail.com>
2019-09-01 21:31:25 -0300

    Now ssr-server deleted

--- ssr-server/config/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const srvConfig = {
-  dev: process.env.NODE_ENV !== 'production',
-  port: process.env.PORT || 8000,
-  apiUrl: process.env.API_URL
-};
-
-module.exports = { srvConfig };
--- ssr-server/routes/readings.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const axios = require('axios');
-
-router.get('/', async (req, res, next) => {
-    
-});
-
-module.exports = router;
